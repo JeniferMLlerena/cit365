475de65ad1383d7ef4fb1b189d65cd5d6b968413
JeniferMLlerena <jen.llerena10@gmail.com>
2018-09-22 18:18:52 -0600

    FINAL VERSION HW01

--- ConsoleApplication/ConsoleApplication/Program.cs
+++ ConsoleApplication/ConsoleApplication/Program.cs
@@ -1,58 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApplication
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             do
             {
+                //Output two WriteLine statements that display those two variables with proper labels (My name is..., I am from...) using String Interpolation.
+                string name, country;
+                Console.Write("What is your name? ");
+                name = Console.ReadLine();
 
-            //Output two WriteLine statements that display those two variables with proper labels (My name is..., I am from...) using String Interpolation.
-            string name, country;
-            Console.Write("What is your name? ");
-            name = Console.ReadLine();
+                Console.Write("Where are you from? ");
+                country = Console.ReadLine();
 
-            Console.Write("Where are you from? ");
-            country = Console.ReadLine();
+                Console.WriteLine($"My name is, {name}! I am from {country} \n");
 
-             Console.WriteLine($"My name is, {name}! I am from {country}");
+                //Output the current date but not the current time.
+                DateTime todayDate = DateTime.Now;
+                Console.WriteLine("The date today " + todayDate.ToString("dd/MM/yyyy") + "\n");
 
-            //Output the current date but not the current time.
-            DateTime todayDate = DateTime.Now;
-            Console.WriteLine("The date today " + todayDate.ToString("dd/MM/yyyy"));
+                //Output the number of days until Christmas with an appropriate label.
+                DateTime christmasDate = new DateTime(2018, 12, 25);
+                TimeSpan daysToChristmas = christmasDate - todayDate;
+                // or you can also use: TimeSpan daysToChristmas = christmasDat.Subtract(todayDate);
+                Console.WriteLine("There are " + daysToChristmas.Days + " left to Christmas. \n");
 
-            //Output the number of days until Christmas with an appropriate label.
-            DateTime christmasDate = new DateTime(2018, 12, 25);
-            TimeSpan daysToChristmas = christmasDate - todayDate;
-            // or you can also use: TimeSpan daysToChristmas = christmasDat.Subtract(todayDate);
-            Console.WriteLine("There are " + daysToChristmas.Days + "left to Christmas");
+                //Add the program example from section 2.1 of the C# and
+                double width, height, woodLength, glassArea;
+                string widthString, heightString;
 
-            //Add the program example from section 2.1 of the C# and
-            double width, height, woodLength, glassArea;
-            string widthString, heightString;
+                //1. Provide appropriate text labels when requesting dimensional input, and
+                Console.Write("Introduce the width of the window: ");
+                widthString = Console.ReadLine();
+                width = double.Parse(widthString);
 
-            //1. Provide appropriate text labels when requesting dimensional input, and
-            Console.Write("Introduce the width of the window: ");
-            widthString = Console.ReadLine();
-            width = double.Parse(widthString);
+                Console.Write("Introduce the height of the window: ");
+                heightString = Console.ReadLine();
+                height = double.Parse(heightString);
 
-            Console.Write("Introduce the height of the window: ");
-            heightString = Console.ReadLine();
-            height = double.Parse(heightString);
+                woodLength = 2 * (width + height) * 3.25;
+                glassArea = 2 * (width * height);
+                Console.WriteLine("The length of the wood is " + woodLength + " feet");
+                Console.WriteLine("The area of the glass is " + glassArea + " square metres \n");
 
-            woodLength = 2 * (width + height) * 3.25;
-            glassArea = 2 * (width * height);
-            Console.WriteLine("The length of the wood is " + woodLength + " feet");
-            Console.WriteLine("The area of the glass is " + glassArea + " square metres");
-
+                //2. Cause the program to pause in the console so that the application does not automatically terminate when launched
                 Console.WriteLine("Do you want to continue (Y/N)? ");
-            } while (Console.ReadLine() != "Y");
+            } while (Console.ReadLine() != "N");
         }
     }
 }
